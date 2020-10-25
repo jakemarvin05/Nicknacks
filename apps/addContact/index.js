38136ed55099b935b00bddd354f0f2479a4fe0d9
calvintwr <calvin14@gmail.com>
2020-10-25 23:31:12 +0800

    disable sending emails for failed to add contact

--- apps/addContact/index.js
+++ apps/addContact/index.js
@@ -8,7 +8,7 @@ const api = `${process.env.CONTACT_BOT_URL}/api/v1/add-contact`
 
 function add(data) {
     axios.post(api, data).catch(err => {
-        err.sendEmail = true
+        err.sendEmail = false
         err.debug = {
             data: data,
             message: 'Failed to add contact'
