8eeca2fcd86ad366f1ed47f624b1347212f4c30c
Calvin Tan <calvin14@gmail.com>
2021-06-19 20:55:49 +0800

    .

--- apps/publicHolidays/readData.js
+++ apps/publicHolidays/readData.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const fs = require('fs')
 
+const dataFolder = path.join(__dirname, 'data')
+
 ph = {}
 
 ph.data = {}
@@ -9,16 +11,26 @@ ph.refresh = function() {
     const data = {}
     const currentYear = (new Date()).getFullYear()
 
-    let files = fs.readdirSync( path.join(__dirname, 'data') ).filter(function (file) {
+    let files = fs.readdirSync(dataFolder).filter(function (file) {
         return (file.indexOf('.') !== 0) && (file.indexOf('.') !== -1) && (file.indexOf('bak') === -1)
     })
 
     files.forEach(function (file) {
-        let json = require( path.join(__dirname, 'data', file) )
+        let json = require( path.join(dataFolder, file) )
         data[ file.replace('.json', '') ] = json
     })
 
     this.data = data
 }
+
+function ensureDirectoryExistence(filePath) {
+    var dirname = path.dirname(filePath);
+    if (fs.existsSync(dirname)) {
+        return true;
+    }
+    ensureDirectoryExistence(dirname);
+    fs.mkdirSync(dirname);
+}
+ensureDirectoryExistence(dataFolder)
 
 module.exports = ph
--- apps/publicHolidays/updateIfRequired.js
+++ apps/publicHolidays/updateIfRequired.js
@@ -6,7 +6,7 @@ const readData = require('./readData.js')
 const phURL = (year) => `https://rjchow.github.io/singapore_public_holidays/api/${year}/data.json`
 const dataFolder = path.join(__dirname, 'data')
 const getHolidays = (year) => {
-    ensureDirectoryExistence(dataFolder)
+
     axios.get( phURL(year) ).then(data => {
         fs.writeFileSync(
             path.join(dataFolder, `${year.toString()}.json`),
@@ -24,14 +24,5 @@ const getHolidays = (year) => {
 function updateIfRequired(data, currentYear = (new Date()).getFullYear()) {
     if (Object.keys(data).indexOf(currentYear.toString()) === -1) getHolidays(currentYear)
     if (Object.keys(data).indexOf((currentYear + 1).toString()) === -1) getHolidays(currentYear + 1)
-}
-
-function ensureDirectoryExistence(filePath) {
-    var dirname = path.dirname(filePath);
-    if (fs.existsSync(dirname)) {
-        return true;
-    }
-    ensureDirectoryExistence(dirname);
-    fs.mkdirSync(dirname);
 }
 module.exports = updateIfRequired
